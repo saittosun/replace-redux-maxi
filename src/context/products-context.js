646cc7d4fd6952079bd67c424fcb862ff633c2d2
tostos <saittosun2@gmail.com>
2020-05-01 14:01:15 +0200

    toggling favorites with the context api

--- src/context/products-context.js
+++ src/context/products-context.js
@@ -2,7 +2,8 @@
 import React, { useState } from 'react';
 
 export const ProductsContext = React.createContext({
-  products: []
+  products: [],
+  toggleFav: (id) => {}
 });
 
 export default props => {
@@ -31,11 +32,32 @@ export default props => {
       description: 'Street style! An orange hat.',
       isFavorite: false
     }
-  ])
+  ]);
+
+  const toggleFavorite = (productId) => {
+    setProductsList(currentProductList => {
+      const prodIndex = currentProductList.findIndex(
+        p => p.id === productId
+      );
+      const newFavStatus = !currentProductList[prodIndex].isFavorite;
+      const updatedProducts = [...currentProductList];
+      updatedProducts[prodIndex] = {
+        ...currentProductList[prodIndex],
+        isFavorite: newFavStatus
+      };
+      return updatedProducts;
+    });
+  };
+
   return (
     // I assign this to my products key here in that object I pass as a value to my products context provider.
     // So now whenever this value here changes whenever we update the state and therefore this component rebuilt. Whenever that happens our provider will get a new value and every child that listens to our provider will be able to get that new value.
-    <ProductsContext.Provider value={{products: productsList}}>
+    <ProductsContext.Provider 
+      value={{
+        products: productsList,
+        // So now this function a pointer at this function is also stored in our context value and now they are for everywhere where we tap into our context we can call this function and then this function will change something in that file will change our state and therefore emit a new state and new value to all components that are listening.
+        toggleFav: toggleFavorite
+      }}>
       {props.children}
     </ProductsContext.Provider>
   )
